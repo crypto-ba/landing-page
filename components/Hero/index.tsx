893d9d932424fcb45cfd878da9febc0c03d18fc4
Rux Crypto.ba <Ruxiol@users.noreply.github.com>
2024-03-17 12:24:02 +0100

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Dobrodošli na Crypto.ba Platformu!
+                Dobrodošli na Crypto.ba!
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
                 Od svog osnivanja u 2016. godini, platforma Crypto.ba je evoluirala iz foruma posvećenog kriptovalutama u sveobuhvatan izvor za obrazovanje o kriptovalutama, platformu za razmjenu istih, te razvojno središte za RXC kriptovalutu, RXCGAMES.com, i projekt CEX.ba.</p>
